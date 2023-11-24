96ebca9b0fb67c2ad9fc2589d22f933e19f87ef2
Luís Felipe <luisrodrigues@bne.com.br>
2023-11-24 16:50:41 -0300

    Enumeradores

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -1,4 +1,6 @@
-﻿namespace Balta.ContentContext;
+﻿using Balta.ContentContext.Enums;
+
+namespace Balta.ContentContext;
 
 public class Course : Content
 {
@@ -9,22 +11,6 @@ public class Course : Content
 
     public string Tag { get; set; }
     public IList<Module> Modules { get; set; }
-}
-
-public class Module
-{
-    public Module()
-    {
-        Lectures = new List<Lecture>();    
-    }
-
-    public int Order { get; set; }
-    public string Title { get; set; }
-    public IList<Lecture> Lectures { get; set; }
-}
-
-public class Lecture
-{
-    public int Order { get; set; }
-    public string Title { get; set; }
+    public int DurationInMinutes { get; set; }
+    public EContentLevel Level { get; set; }
 }
