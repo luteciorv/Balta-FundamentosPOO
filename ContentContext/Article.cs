7a67956eea0b404195f7162c71a95b59239cfa77
Luís Felipe <luisrodrigues@bne.com.br>
2023-11-27 10:13:50 -0300

    Criando a classe de notificação

--- ContentContext/Article.cs
+++ ContentContext/Article.cs
@@ -1,7 +1,11 @@
-﻿namespace Balta.ContentContext;
+﻿using Balta.NotificationContext;
+
+namespace Balta.ContentContext;
 
 public class Article : Content
 {
+    public  IList<Notification> Notifications { get; set; }
+
     public Article(string title, string url) : base(title, url)
     {
     }
